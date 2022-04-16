bd835fa6b5edadde302b824c61a8429ab2238aa0
Nilender-Andrey <nilenderav@mail.ru>
2022-04-16 09:44:15 +0400

    feat: add sort posts

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
-    <custom-button @click="showDialog">Создать пост</custom-button>
+
+    <div class="app__btns">
+      <custom-button class="btn_create-post" @click="showDialog"
+        >Создать пост</custom-button
+      >
+
+      <my-select v-model="selectedSort" :options="sortOptions" />
+    </div>
+
     <custom-dialog v-model:show="dialogVisible">
       <!-- подписываемся на событие -->
       <PostForm @create="createPost" />
     </custom-dialog>
 
     <!-- v-bind - полная, : - коротка форма -->
-    <PostList :posts="posts" @remove="removePost" />
+    <PostList
+      v-if="!isPostsLoading"
+      :posts="sortedPosts"
+      @remove="removePost"
+    />
+
+    <div v-else>Идет загрузка...</div>
   </div>
 </template>
 
 <script>
 import PostForm from '@/components/PostForm';
 import PostList from '@/components/PostList';
+import axios from 'axios';
 
 export default {
   components: {
@@ -24,24 +39,14 @@ export default {
 
   data() {
     return {
-      posts: [
-        {
-          id: 1,
-          title: 'Название_1',
-          body: 'Описание_1',
-        },
-        {
-          id: 2,
-          title: 'Название_2',
-          body: 'Описание_2',
-        },
-        {
-          id: 3,
-          title: 'Название_3',
-          body: 'Описание_3',
-        },
-      ],
+      posts: [],
       dialogVisible: false,
+      isPostsLoading: false,
+      selectedSort: '',
+      sortOptions: [
+        { value: 'title', name: 'По названию' },
+        { value: 'body', name: 'По содержимому' },
+      ],
     };
   },
 
@@ -56,7 +61,43 @@ export default {
     showDialog() {
       this.dialogVisible = true;
     },
+
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true;
+        const response = await axios.get(
+          'https://jsonplaceholder.typicode.com/posts?_limit=10',
+        );
+        this.posts = response.data;
+      } catch (error) {
+        alert('Ошибка');
+      } finally {
+        this.isPostsLoading = false;
+      }
+    },
   },
+
+  mounted() {
+    this.fetchPosts();
+  },
+  computed: {
+    sortedPosts() {
+      if (this.selectedSort) {
+        return [...this.posts].sort((post1, post2) =>
+          post1[this.selectedSort].localeCompare(post2[this.selectedSort]),
+        );
+      }
+      return this.posts;
+    },
+  },
+
+  /*  watch: {
+    selectedSort(newValue) {
+      this.posts.sort((post1, post2) =>
+        post1[newValue].localeCompare(post2[newValue]),
+      );
+    },
+  }, */
 };
 </script>
 
@@ -71,5 +112,12 @@ export default {
   max-width: 800px;
   margin: 0 auto;
   padding: 20px;
+}
+
+.app__btns {
+  display: flex;
+  justify-content: space-between;
+
+  margin: 15px 0;
 }
 </style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,18 +1,22 @@
 <template>
   <div v-if="posts.length > 0">
-    <h4>Список пользователей</h4>
+    <h4 class="title-posts-list">Список пользователей</h4>
 
-    <PostItem
-      v-for="post in posts"
-      :post="post"
-      :key="post.id"
-      @remove="$emit('remove', post)"
-    />
+    <transition-group name="post-list">
+      <post-item
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <h2 v-else style="color: red">Список постов пуст</h2>
 </template>
+
 <script>
 import PostItem from '@/components/PostItem';
+
 export default {
   components: { PostItem },
 
@@ -24,4 +28,24 @@ export default {
   },
 };
 </script>
-<style></style>
+
+<style scope>
+.title-posts-list {
+  margin-top: 20px;
+}
+
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+
+.post-list-move {
+  transition: transform 0.4s ease;
+}
+</style>
